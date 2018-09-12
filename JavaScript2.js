ffe561225b4eadb39db56b4b151208f8ffa3a9f0
helen1717 <39730568+helen1717@users.noreply.github.com>
2018-09-12 10:17:06 +0300

    Add files via upload

---
+++ JavaScript2.js
@@ -0,0 +1,58 @@
+﻿function $() 
+{
+var elements = new Array();
+for (var i = 0; i < arguments.length; i++)
+{var element = arguments[i];
+if (typeof element == 'string')
+element = document.getElementById(element);
+if (arguments.length == 1)
+return element;
+elements.push(element);
+}
+return elements;}
+
+
+
+function a1()
+{
+	$("bb").style.backgroundColor='rgb(0, 0, 0)';
+}
+
+function b1()
+{
+	$("bb").style.backgroundColor='red';
+}
+
+function c1()
+{
+	$("bb").style.backgroundColor='rgb(255, 127, 80)';
+}
+
+function d1()
+{
+	$("bb").style.backgroundColor='rgb(255, 255, 0)';
+}
+
+function e1()
+{
+	$("bb").style.backgroundColor='rgb(0, 250, 154)';
+}
+
+function h1()
+{
+	$("bb").style.backgroundColor='rgb(0, 255, 255)';
+}
+function s1()
+{
+	$("bb").style.backgroundColor='rgb(30, 144, 255)';
+}
+function o1()
+{
+	$("bb").style.backgroundColor='rgb(138, 43, 226)';
+}
+
+
+function f1()
+{
+	$("txt").value='ПРИВЕТ, МИР!';
+}	
