613987806dae185ac00fd79cbff6950f4ae1b071
helen1717 <39730568+helen1717@users.noreply.github.com>
2018-11-06 17:31:23 +0200

    Add files via upload

---
+++ Script3.3.js
@@ -0,0 +1,41 @@
+﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f2()
+{       K=parseFloat($('tx2').value);
+	N=parseFloat($('tx1').value);
+S=1;
+for(i=2;i<=N;i++)
+{a=i;
+a=Math.pow(a,K); 
+S=S+a;
+}
+$('tx3').value=''+S;
+}
+
+function f3()
+{       A=parseFloat($('tx4').value);
+	B=parseFloat($('tx5').value);
+result='';
+	for(var i=A; i<=B; i++)
+	
+{
+		
+for(var j=1; j<= i; j++)
+
+			result=result+i;
+	
+}
+	
+$('tx6').value=result;
+
+}
