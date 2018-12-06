447d73447ed540190b910edcbf934ed77841ff8b
helen1717 <39730568+helen1717@users.noreply.github.com>
2018-12-06 15:03:27 +0200

    Add files via upload

---
+++ JavaScript34.js
@@ -0,0 +1,35 @@
+﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f2()
+{
+	
+		
+alert('33333333333333');
+var A = parseInt(document.getElementById('A').value),
+ K = 0, Summ = 0;
+			
+
+
+while (Summ < A) Summ += (1/ ++K);
+		
+		
+
+$('K').value = K-1;
+		
+
+$('Summ').value = Summ-1/K;
+
+
+	
+	
+}
