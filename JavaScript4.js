b2f2226b490c1029084fcf2aafdc5c2bc7f8a8e4
helen1717 <39730568+helen1717@users.noreply.github.com>
2018-12-20 21:47:48 +0200

    Add files via upload

---
+++ JavaScript4.js
@@ -0,0 +1,33 @@
+/*function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; } 
+*/
+ 
+
+
+function f(x) 
+{var x=document.getElementById('In').value;
+ var a = '1234567890';
+ fff=1;
+   for (var i = 0; i < x.length; i++)
+   {
+      b=x.charAt(i);
+    alert(b);
+    if(a.indexOf(b)<0)
+     fff=0;
+   }
+ if(fff)
+      {return 'Okay';}
+   else
+      {return 'Fail';}
+ 
+
+}
